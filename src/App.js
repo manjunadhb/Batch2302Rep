3f64801db0069e502599574b5f0d0e79f3ca9890
MOHD JUNAID ALI <junaidali3089@gmail.com>
2023-05-11 20:29:25 +0530

    added content about globel warming

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ function App() {
     <div className="App">
       <h1>About Cricket</h1>
       <p>Cricket is a bat-and-ball game played between two teams of eleven players on a field at the centre of which is a 22-yard 20-metre pitch with a wicket at each end, each comprising two bails balanced on three stumps. The batting side scores runs by striking the ball bowled at one of the wickets with the bat and then running between the wickets, while the bowling and fielding side tries to prevent this by preventing the ball from leaving the field, and getting the ball to either wicket and dismiss each batter so they are "out". Means of dismissal include being bowled, when the ball hits the stumps and dislodges the bails, and by the fielding side either catching the ball after it is hit by the bat, but before it hits the ground, or hitting a wicket with the ball before a batter can cross the crease in front of the wicket. When ten batters have been dismissed, the innings ends and the teams swap roles. The game is adjudicated by two umpires, aided by a third umpire and match referee in international matches. They communicate with two off-field scorers who record the match's statistical information.</p>
+
+      <h1>Global warming</h1>
+      <p>Global warming is the long-term heating of Earth's surface observed since the pre-industrial period between 1850 and 1900 due to human activities, primarily fossil fuel burning, which increases heat-trapping greenhouse gas levels in Earth's atmosphere. This term is not interchangeable with the term "climate change."</p>
       
     </div>
   );
